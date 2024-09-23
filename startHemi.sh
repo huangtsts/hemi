32989007f8618cc3363013e067a28052f0105afc
huangtsts <huangtsts@gmail.com>
2024-09-23 22:30:14 +0800

    Update startHemi.sh

--- startHemi.sh
+++ startHemi.sh
@@ -100,7 +100,8 @@ setup_environment() {
 # 功能3：启动 popmd（使用 screen）
 start_popmd() {
     cd "$HOME/heminetwork"
-    ./popmd
+    screen -dmS popmd_session ./popmd
+    echo "popmd 已启动。你可以通过 'screen -r popmd_session' 查看运行中的进程。"
 }
 
 # 功能4：查看日志（使用 screen）
